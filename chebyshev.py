1a8df13b12d313d32e448668c2e52bcb675127d3
Dan Greenwald <greenw2@gmail.com>
2017-11-07 16:08:10 -0500

    updates

--- chebyshev.py
+++ chebyshev.py
@@ -1,50 +1,5 @@
 import numpy as np
-
-def cartesian(arrays, out=None):
-    """
-    Generate a cartesian product of input arrays.
-    Parameters
-    ----------
-    arrays : list of array-like
-        1-D arrays to form the cartesian product of.
-    out : ndarray
-        Array to place the cartesian product in.
-    Returns
-    -------
-    out : ndarray
-        2-D array of shape (M, len(arrays)) containing cartesian products
-        formed of input arrays.
-    Examples
-    --------
-    >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
-    array([[1, 4, 6],
-           [1, 4, 7],
-           [1, 5, 6],
-           [1, 5, 7],
-           [2, 4, 6],
-           [2, 4, 7],
-           [2, 5, 6],
-           [2, 5, 7],
-           [3, 4, 6],
-           [3, 4, 7],
-           [3, 5, 6],
-           [3, 5, 7]])
-    """
-
-    arrays = [np.asarray(x) for x in arrays]
-    dtype = arrays[0].dtype
-
-    n = np.prod([x.size for x in arrays])
-    if out is None:
-        out = np.zeros([n, len(arrays)], dtype=dtype)
-
-    m = n // arrays[0].size
-    out[:,0] = np.repeat(arrays[0], m)
-    if arrays[1:]:
-        cartesian(arrays[1:], out=out[0:m,1:])
-        for j in range(1, arrays[0].size):
-            out[j*m:(j+1)*m,1:] = out[0:m,1:]
-    return out
+from py_tools.utilities import cartesian
 
 def grid(n_pts):
 
@@ -141,11 +96,28 @@ class TensorChebFcn:
         vals = fcn(self.scaled_grid)
         self.fit_vals(vals)
         
-    def evaluate(self, x):
+    def evaluate(self, x, chunksize=100000):
         
+        x = np.minimum(x, self.ub)
+        x = np.maximum(x, self.lb)
         x_grid = self.scale_to_grid(x)
-        Tx = tensor(x_grid, self.n_vec)
-        return np.dot(Tx, self.coeffs)
+
+        Nx = x_grid.shape[0]
+        if Nx > chunksize:
+            vals = np.zeros(Nx)
+            count = 0
+            while count < Nx:
+                vals[count : count + chunksize] = eval_tensor(
+                    x_grid[count : count + chunksize, :],
+                    self.n_vec, self.coeffs
+                )
+                count += chunksize
+        else:
+            # Tx = tensor(x_grid, self.n_vec)
+            # vals= np.dot(Tx, self.coeffs) 
+            vals = eval_tensor(x_grid, self.n_vec, self.coeffs)
+
+        return vals
 
     def scale_to_grid(self, vals):
 
@@ -154,3 +126,8 @@ class TensorChebFcn:
     def scale_from_grid(self, vals):
 
         return self.a_from_grid + (self.b_from_grid * vals)
+
+def eval_tensor(x, n_vec, coeffs):
+
+    Tx = tensor(x, n_vec)
+    return np.dot(Tx, coeffs)
--- utilities.py
+++ utilities.py
@@ -85,3 +85,63 @@ def toc(start, display=True):
     elapsed = time() - start
     print("Time elapsed: {:4.3g} seconds".format(elapsed))
     return elapsed
+
+def cartesian(arrays, out=None):
+    """
+    Generate a cartesian product of input arrays.
+    Parameters
+    ----------
+    arrays : list of array-like
+        1-D arrays to form the cartesian product of.
+    out : ndarray
+        Array to place the cartesian product in.
+    Returns
+    -------
+    out : ndarray
+        2-D array of shape (M, len(arrays)) containing cartesian products
+        formed of input arrays.
+    Examples
+    --------
+    >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
+    array([[1, 4, 6],
+           [1, 4, 7],
+           [1, 5, 6],
+           [1, 5, 7],
+           [2, 4, 6],
+           [2, 4, 7],
+           [2, 5, 6],
+           [2, 5, 7],
+           [3, 4, 6],
+           [3, 4, 7],
+           [3, 5, 6],
+           [3, 5, 7]])
+    """
+
+    arrays = [np.asarray(x) for x in arrays]
+    dtype = arrays[0].dtype
+
+    n = np.prod([x.size for x in arrays])
+    if out is None:
+        out = np.zeros([n, len(arrays)], dtype=dtype)
+
+    m = n // arrays[0].size
+    out[:,0] = np.repeat(arrays[0], m)
+    if arrays[1:]:
+        cartesian(arrays[1:], out=out[0:m,1:])
+        for j in range(1, arrays[0].size):
+            out[j*m:(j+1)*m,1:] = out[0:m,1:]
+    return out
+
+def cartesian_matrices(A, B):
+
+    Nx = A.shape[0] * B.shape[0]
+    Ny = A.shape[1] * B.shape[1]
+
+    C = np.zeros((Nx, Ny, 2))
+
+    for ii, jj in itertools.product(range(A.shape[0]), range(B.shape[0])):
+
+        kk = B.shape[0] * ii + jj 
+        C[kk, :, :] = cartesian((A[ii, :], B[jj, :]))
+
+    return C
