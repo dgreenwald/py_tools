778dd2cb054ba5e95d203afa621fac3d39efc7a0
Dan Greenwald <dlg@mit.edu>
2018-07-26 10:35:58 -0400

    minor updates to prior module (fixed bug) and time_series, added kalman filter module, may not be working

--- prior.py
+++ prior.py
@@ -7,7 +7,7 @@ GAMMA = 2
 INV_GAMMA = 3
 NORM = 4
 
-def get_prior(prior_type, mean=None, sd=None, params=None):
+def get_prior(prior_type, mean=None, sd=None, mode=None, params=None):
 
     prior_num_dict = {
         'beta' : BETA,
@@ -20,12 +20,16 @@ def get_prior(prior_type, mean=None, sd=None, params=None):
         prior_num = prior_type
     elif isinstance(prior_type, str):
         prior_num = prior_num_dict[prior_type]
+    elif prior_type is None:
+        return None
     else:
         print("Bad prior type")
         raise Exception
 
     if params is None:
-        assert (mean is not None) or (sd is not None)
+        assert ((mean is not None) or (mode is not None)) and (sd is not None)
+        assert (mean is None) or (mode is None)
+
         if prior_num == BETA:
             alp = (1.0 - mean) * ((mean / sd) ** 2) - mean
             bet = (1.0 - mean) * alp / mean
@@ -35,15 +39,17 @@ def get_prior(prior_type, mean=None, sd=None, params=None):
             k = mean / the
             return st.gamma(k, scale=the)
         elif prior_num == INV_GAMMA:
+            # raise Exception
             alp = 2 + ((mean / sd) ** 2)  
-            bet = mean * (alp - 1)
+            if mode is not None:
+                bet = mean * (alp + 1)
+            else:
+                bet = mean * (alp - 1)
             return st.invgamma(alp, scale=bet)
         elif prior_num == NORM:
             return st.norm(loc=mean, scale=sd)
         else:
            raise Exception
-
-    return (prior_num, params)
 
 class Prior:
     """Bayesian prior"""
@@ -55,4 +61,4 @@ class Prior:
         self.dists.append(get_prior(prior_type, *args, **kwargs))
 
     def logpdf(self, vals):
-        return np.sum([dist.logpdf(val) for dist, val in zip(self.dists, vals)])
+        return np.sum([dist.logpdf(val) for dist, val in zip(self.dists, vals) if dist is not None])
