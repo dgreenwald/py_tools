856892bbc7e97ed50d7ce996a8752715aea33ef2
Dan Greenwald <dlg@mit.edu>
2019-01-30 18:24:45 -0500

    new warning for scrape.py

--- scrape.py
+++ scrape.py
@@ -1,15 +1,24 @@
 from bs4 import BeautifulSoup
 import nltk
 import requests
+import time
 
-def get_soup(url):
+def get_soup(url, delay=1e-4):
     """Strip HTML tags from a URL"""
     
+    # req = requests.get(url, headers={'User-agent': 'Super Bot 9000'})
     req = requests.get(url)
     if req.status_code == 200:
         html = req.content
         return BeautifulSoup(html, 'lxml')
+    # elif req.status_code == 429:
+        # print("429 Error")
+        # print("got 429, delaying {:g}s".format(delay))
+        # time.sleep(delay)
+        # new_delay = 2.0 * delay
+        # return get_soup(url, delay=new_delay)
     else:
+        print("{:d} Error".format(req.status_code))
         return None
 
 def stripHTML(url):
