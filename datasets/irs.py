cb846cf34d10caad9563377c9e6d41d2e2da6fd4
Dan Greenwald <greenw2@gmail.com>
2019-05-23 19:32:32 -0400

    more updates to IRS code

--- datasets/irs.py
+++ datasets/irs.py
@@ -6,7 +6,6 @@ import re
 
 from . import defaults
 default_dir = defaults.base_dir() + 'irs/'
-# data_dir = '/home/dan/data/irs/'
 
 def double_list(these_items):
 
@@ -37,10 +36,6 @@ def compute_fips(df_t):
     df_t = df_t.loc[ix, :]
         
     df_t['fips'] = df_t['countyfips'] + 1000 * df_t['statefips']
-    
-#    counts = df_t['fips'].value_counts()
-#    if np.any(counts > 1):
-#        print(counts.loc[counts > 1])
 
     return df_t
 
@@ -66,7 +61,7 @@ def load_county_year(year, data_dir=default_dir, reimport=False):
         elif year == 2010:
             df_t = import_county_year_2010(year, data_dir=default_dir)
         else:
-            df_t = import_county_year_from_2011(year, data_dir=default_dir)
+            df_t = import_geo_year_from_2011(year, 'county', data_dir=default_dir)
 
         df_t.to_pickle(pkl_file)
     else:
@@ -74,7 +69,16 @@ def load_county_year(year, data_dir=default_dir, reimport=False):
 
     return df_t
 
-def import_county_year_from_2011(year, data_dir=default_dir):
+def import_geo_year_from_2011(year, geo, data_dir=default_dir):
+
+    if geo == 'county':
+        file_stub = 'incyallagi'
+        group_var = 'fips'
+    elif geo == 'zip':
+        file_stub = 'zpallagi'
+        group_var = 'zip'
+    else:
+        raise Exception
 
     col_map = {
         'n1' : 'n_returns',
@@ -120,27 +124,31 @@ def import_county_year_from_2011(year, data_dir=default_dir):
         '11901' : 'tax_due_at_filing',
         '11902' : 'overpayments_refunded',
     }
+    
+    if geo == 'zip':
+        col_map['zipcode'] = 'zip'
 
     # add number and level
     col_map.update({'n' + key : 'n_' + val for key, val in n_map.items()})
     col_map.update({'a' + key : val for key, val in n_map.items()})
 
-    final_map = { key.upper() : val for key, val in col_map.items() } 
+    final_map = { key.lower() : val for key, val in col_map.items() } 
 
     short_year = str(year)[2:]
-    df_by_cat = pd.read_csv(data_dir + 'county/' + short_year + 'incyallagi.csv',
-                     encoding='latin1').rename(columns=final_map)
+    df_by_cat = pd.read_csv(data_dir + geo + '/' + short_year + file_stub + '.csv',
+                     encoding='latin1')
     df_by_cat = df_by_cat.rename(columns={col : col.lower() for col in df_by_cat.columns})
+    df_by_cat = df_by_cat.rename(columns=final_map)
+    
+    ix = df_by_cat['n_returns'] >= 1
+    df_by_cat = df_by_cat.loc[ix, :]
 
-    df_by_cat = compute_fips(df_by_cat)
+    if geo == 'county':
+        df_by_cat = compute_fips(df_by_cat)
+    elif geo == 'zip':
+        df_by_cat['zip'] = df_by_cat['zip'].astype(np.int64)
 
-    # for var in ['countyfips', 'statefips']:
-        # df_by_cat[var] = pd.to_numeric(df_by_cat[var], errors='coerce').astype(np.int64)
-
-    # df_by_cat = df_by_cat.dropna(subset=['countyfips', 'statefips'])
-    # df_by_cat['fips'] = df_by_cat['countyfips'] + 1000 * df_by_cat['statefips']
-
-    df_t = df_by_cat.groupby(['fips']).sum().drop(['agi_stub'], axis=1).reset_index()
+    df_t = df_by_cat.groupby([group_var]).sum().drop(['agi_stub'], axis=1).reset_index()
 
     df_t['date'] = '{0:d}-01-01'.format(year)
 
@@ -179,8 +187,6 @@ def import_county_year_2010(year, data_dir=default_dir):
 def import_county_year_to_2009(year, data_dir=default_dir):
 
     year_dir = data_dir + 'county/' + str(year) + 'CountyIncome/'
-    
-    # if year <= 2009:
 
     names = ['DROP', 'statefips', 'countyfips', 'county', 'n_returns',
              'n_exemptions', 'agi', 'wagesal', 'dividends', 'interest']
@@ -197,29 +203,6 @@ def import_county_year_to_2009(year, data_dir=default_dir):
         skiprows = 8
         names = names[1:]
         drop_list = []
-        
-    # elif year == 2010:
-
-        # names = [
-            # 'countyfips', 'categories', 'n_returns', 'n_joint', 'n_prepared',
-            # 'n_exemptions', 'n_dependents', 'agi',
-        # ]
-        
-        # for item in ['wagesal', 'interest', 'dividends', 'qualified_dividends', 'business']:
-            # names += ['n_' + item, item]
-
-        # names += ['n_farm']
-
-        # for item in ['cap_gain', 'retirement', 'pension', 'unemp', 'social', 
-                # 'self_emp_ret', 'itemized', 'state_local_inc', 'state_local_sales', 'real_estate_tax', 
-                # 'taxes_paid', 'mort_int_paid', 'contributions', 'taxable_income', 'tax_credits',
-                # 'res_energy_credit', 'child_tax_credit', 'child_dep_care_credit', 'add_child_credit', 'eitc',
-                # 'excess_eitc', 'alt_minimum_tax', 'income_tax', 'tax_liability', 'tax_due_at_filing',
-                # 'overpayments_refunded']:
-            # names += ['n_' + item, item]
-
-        # skiprows = 15
-        # drop_list = []
 
     target_cols = len(names)
     df_t = pd.concat([
@@ -230,30 +213,6 @@ def import_county_year_to_2009(year, data_dir=default_dir):
 
     df_t = df_t.drop(drop_list, axis=1)
     df_t = compute_fips(df_t)
-
-    # df_t['countyfips'] = pd.to_numeric(df_t['countyfips'], errors='coerce')
-    # df_t['statefips'] = pd.to_numeric(df_t['statefips'], errors='coerce')
-
-    # df_t = df_t.drop(drop_list, axis=1).dropna(subset=['countyfips', 'statefips'])
-    # for name in ['countyfips', 'statefips']:
-        # df_t[name] = df_t[name].astype(np.int64)
-
-    # df_t = df_t.loc[df_t['countyfips'] > 0 & df_t['statefips'] > 0, :]
-        
-    # df_t['fips'] = df_t['countyfips'] + 1000 * df_t['statefips']
-
-    # Drop binned categories
-    # if year >= 2010:
-
-        # drop_cats = ['Under $1', '$1 under $25,000', '$25,000 under $50,000',
-                     # '$50,000 under $75,000', '$75,000 under $100,000',
-                     # '$100,000 under $200,000', '$200,000 or more']
-
-        # ix = np.zeros(len(df_t), dtype=bool)
-        # for cat in drop_cats:
-            # ix = np.logical_or(ix, df_t['categories'] == cat)
-        # df_t = df_t.drop(df_t.index[ix])
-        # df_t = df_t.dropna(subset=['categories']).drop('categories', axis=1)
     
     df_t['date'] = '{0:d}-01-01'.format(year)
     return df_t
@@ -291,15 +250,15 @@ def load_state_zip_year(filename, year):
     df = pd.read_excel(filename, 
                       skiprows=skiprows, 
                        header=None)
-    pattern = re.compile(r'\s*\d\d\d\d\d\s*')
+    zip_pattern = re.compile(r'\s*\d\d\d\d\d\s*')
     
-    if year <= 2006:
+    if year <= 2005:
 
         row_names = df[0].astype(str).values
         
         ix = np.zeros(len(df), dtype=bool)
         for ii, name in enumerate(row_names):
-            if pattern.match(name):
+            if zip_pattern.match(name):
                 ix[ii] = True
             
         df = df.loc[ix, :]
@@ -318,7 +277,7 @@ def load_state_zip_year(filename, year):
 
         ix = np.zeros(len(df), dtype=bool)
         for ii, (bin_name, zip_name) in enumerate(zip(bin_names, zip_names)):
-            if bin_name[0] != '$' and pattern.match(zip_name):
+            if (not bin_name[0] in ['$', 'U']) and zip_pattern.match(zip_name):
                 ix[ii] = True
 
         df = df.loc[ix, :].drop(columns=[ix_bin])
@@ -357,7 +316,7 @@ def import_zip_year_to_2010(year, data_dir=default_dir):
         names = ['zip', 'n_returns', 'n_exemptions', 'n_dependents', 'agi']
         names += double_list(['wagesal', 'interest', 'dividends', 'cap_gain', 'schedule_c',
                               'schedule_f', 'ira_deduction', 'pension_deduction'])
-        names += triple_list(['itemized', 'contributions', 'taxes_paids'])
+        names += triple_list(['itemized', 'contributions', 'taxes_paid'])
         
         names += double_list(['alt_minimum_tax', 'income_tax_before_credits', 'tax_liability', 'eitc'])
 
@@ -407,24 +366,18 @@ def import_zip_year_to_2010(year, data_dir=default_dir):
             'tax_due_at_filing', 'overpayments_refunded',
         ])
 
-    if year <= 2006:
-        df_t = pd.concat([
-            load_state_zip_year(filename, year)
-            for filename in glob.glob(year_dir + '*.xls')
-        ])
-    else:
+    if False:
         print('TESTING')
         file_list = list(glob.glob(year_dir + '*.xls'))
         df_t = pd.concat([
                 load_state_zip_year(filename, year)
-                for filename in file_list[:3]
+                for filename in file_list[:1]
                 ])
-#        df_t = pd.concat([
-#            load_state_zip_year(filename, year)
-#            for filename in glob.glob(year_dir + '*.xls')
-#        ])
-
-    print(df_t.head(10))
+    else:
+        df_t = pd.concat([
+            load_state_zip_year(filename, year)
+            for filename in glob.glob(year_dir + '*.xls')
+        ])
 
     df_t = df_t.loc[:, :len(names)-1]
     df_t.columns = names
@@ -432,7 +385,6 @@ def import_zip_year_to_2010(year, data_dir=default_dir):
     df_t = df_t.apply(pd.to_numeric, errors='coerce')
 
     df_t['date'] = str(year) + '-01-01'
-    df_t['date'] = pd.to_datetime(df_t['date'])
 
     df_t['zip'] = df_t['zip'].astype(np.int64)
         
@@ -446,11 +398,50 @@ def load_zip_year(year, data_dir=default_dir, reimport=False):
         print("Loading year {}".format(year))
         if year <= 2010:
             df_t = import_zip_year_to_2010(year, data_dir=default_dir)
+        elif year <= 2016:
+            df_t = import_geo_year_from_2011(year, 'zip', data_dir=default_dir)
         else:
             raise Exception
+            
+        df_t['date'] = pd.to_datetime(df_t['date'])
+        
+        ix = (df_t['zip'] >= 501) & (df_t['zip'] <= 99950)
+        df_t = df_t.loc[ix, :]
+
+        df_t['count'] = 1
+        df_count = df_t.groupby('zip')['count'].count()
+        if np.amax(df_count) > 1:
+    
+            print("Duplicates detected")
+            ix = df_count > 1
+            print(df_count.loc[ix])
+            bad_zips = df_count.index[ix]
+    
+            bad_ix = np.any(df_t['zip'].values[:, np.newaxis] == bad_zips.values[np.newaxis, :], axis=1)
+            print(df_t.loc[bad_ix, :])
+    
+        df_t = df_t.groupby('zip').sum().reset_index()
 
         df_t.to_pickle(pkl_file)
     else:
         df_t = pd.read_pickle(pkl_file) 
 
     return df_t
+
+def load_zip(data_dir=default_dir, reimport=False, reimport_year=False):
+
+    zip_years = [1998, 2001, 2002] + list(range(2004, 2011))
+
+    pkl_file = data_dir + 'zip/irs_zip.pkl'
+    if reimport or not os.path.exists(pkl_file):
+
+        df = pd.concat((load_zip_year(year, reimport=reimport_year) for year in zip_years), sort=True)
+        df['date'] = pd.to_datetime(df['date'])
+        df = df.set_index(['zip', 'date']).sort_index()
+        df.to_pickle(pkl_file)
+
+    else:
+
+        df = pd.read_pickle(pkl_file)
+
+    return df
