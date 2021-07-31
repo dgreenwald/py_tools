40acdaef3b197e8abb67d2a503df9c47fb26e7f2
Dan Greenwald <greenw2@gmail.com>
2021-07-30 23:17:51 -0400

    minor changes

--- debug.py
+++ debug.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tabulate import tabulate
 
-def disp(mat):
+def disp(mat, floatfmt='.4f'):
     """Display in Matlab form"""
 
     if len(mat.shape) == 1:
-        print(tabulate(mat[:, np.newaxis], floatfmt='.4f'))
+        print(tabulate(mat[:, np.newaxis], floatfmt=floatfmt))
     else:
-        print(tabulate(mat, floatfmt='.4f'))
+        print(tabulate(mat, floatfmt=floatfmt))
     return None
 
 def plow(x):
