9b781e4135b75763fd4cc86f4ccc52ab5876fcf9
Dan Greenwald <greenw2@gmail.com>
2025-08-06 12:19:26 -0400

    various updates

--- datasets/spf.py
+++ datasets/spf.py
@@ -25,7 +25,11 @@ def load(table, data_dir=default_dir+'/spf/', reimport=False):
     
     return df
 
-def load_master(table, data_dir=default_dir + '/spf/', reimport=False, growth=False):
+def load_table_from_master(table=None, data_dir=default_dir + '/spf/', 
+                reimport=False, growth=False, vintage=None):
+    
+    if vintage is not None:
+        data_dir += f'{vintage}/'
     
     if growth:
         base_name = 'meanGrowth'
@@ -52,4 +56,17 @@ def load_master(table, data_dir=default_dir + '/spf/', reimport=False, growth=Fa
         
         df = pd.read_pickle(pkl_file)
         
+    return df
+
+def load_tables_from_master(tables=None, data_dir=default_dir + '/spf/', 
+                reimport=False, growth=False, vintage=None):
+    
+    df_list = []
+    for table in tables:
+        this_df = load_table_from_master(
+            table=table, data_dir=data_dir, reimport=reimport, growth=growth, 
+            vintage=vintage)
+        df_list.append(this_df)
+        
+    df = pd.concat(df_list, axis=1)
     return df
