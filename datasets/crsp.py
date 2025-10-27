9f26b90bb1d69bf4554f887c3c48968ced386bf6
Dan Greenwald <greenw2@gmail.com>
2025-10-27 17:10:13 -0400

    update to crsp

--- datasets/crsp.py
+++ datasets/crsp.py
@@ -28,3 +28,39 @@ def load(data_dir=default_dir, vintage='2017', **kwargs):
     df['pd_q'] = df['p'] - np.log(df['D'])
 
     return df
+
+def load_new(data_dir=default_dir, vintage='2017', freq='Q', **kwargs):
+
+    version = vintage
+    if freq == 'M':
+        version += '_' + freq
+    elif freq != 'Q':
+        raise Exception
+
+    infile = 'crsp_{}.csv'.format(version)
+    df = pd.read_csv(data_dir + infile, parse_dates=['caldt'])
+    df['date'] = df['caldt'] + pd.tseries.frequencies.to_offset(freq + 'S')
+    df = df.set_index('date')
+
+    df['P'] = (df['vwretx'] + 1.0).cumprod()
+    df['D'] = np.hstack((np.nan, df['P'][:-1])) * (df['vwretd'] - df['vwretx'])
+
+    if freq == 'Q':
+        window = 4
+    elif freq == 'M':
+        window = 12
+        
+    df['D_ann'] = df['D'].rolling(window).sum()
+
+    df['p'] = np.log(df['P'])
+    df['d_ann'] = np.log(df['D_ann'])
+    
+    df['Re'] = (df['P'] + df['D']) / df['P'].shift()
+    
+    df['re'] = np.log(df['Re'])
+    
+    df['PD'] = df['P'] / df['D_ann']
+    df['pd'] = df['p'] - df['d_ann']
+    df['pd_q'] = df['p'] - np.log(df['D'])
+
+    return df
